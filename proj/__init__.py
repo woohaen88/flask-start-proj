72b48053d531a65c02f5caff3dcef0f4cf541910
woohaen88 <woohaen88@gmail.com>
2021-10-14 11:59:53 +0000

    템플릿 엔진 기초: 정적파일 추가 및 디버그 환경 캐시제거

--- proj/__init__.py
+++ proj/__init__.py
@@ -4,6 +4,9 @@ from flask import render_template
 def create_app():
     app = Flask(__name__)
 
+    if app.config['DEBUG']:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
     @app.route('/')
     def index():
         return render_template('index.html')
