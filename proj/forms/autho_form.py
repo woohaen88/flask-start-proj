5e36d2201747596c1aa5e096331dab2c81e3e364
woohaen88 <woohaen88@gmail.com>
2021-10-19 01:51:56 +0000

    Flask WTF으로 로그인_회원가입 폼 레이아웃 작성

---
+++ proj/forms/autho_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+class LoginForm(FlaskForm):
+    user_id = StringField('User Id', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class RegisterForm(LoginForm):
+    password = PasswordField(
+        'Password', 
+        validators=[DataRequired(), EqualTo(
+            'repassword',
+            message='Password must match'
+        )])
+
+    repassword = PasswordField(
+        'confirm password',
+        validators=[DataRequired()]
+    )
+
+    user_name = StringField('User Name', validators=[DataRequired()])
